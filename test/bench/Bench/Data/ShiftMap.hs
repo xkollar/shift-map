2376fdd7caf26d0df54d7637d00dc1f59d74fd65
Matej Kollar <208115@mail.muni.cz>
2020-04-25 14:37:39 +0100

    Bit more stable benchmarks

--- test/bench/Bench/Data/ShiftMap.hs
+++ test/bench/Bench/Data/ShiftMap.hs
@@ -21,8 +21,8 @@ import Data.ShiftMap
 import DeepseqInstance ()
 
 
-setupEnv :: IO ()
-setupEnv = pure ()
+setupEnv :: IO (ShiftMap ())
+setupEnv = pure fixture2to16
 
 fixture1000 :: ShiftMap ()
 fixture1000 = foldl' (\ m n -> shiftInsert n () m) empty [1..1000]
@@ -31,10 +31,10 @@ fixture2to16 :: ShiftMap ()
 fixture2to16 = foldl' (\ m n -> shiftInsert n () m) empty [1..2^(16::Int)-1]
 
 benchmarks :: Benchmark
-benchmarks = env setupEnv $ \_ -> bgroup "Bench.Data.ShiftMap"
+benchmarks = env setupEnv $ \ bigExample -> bgroup "Bench.Data.ShiftMap"
     [ bench "1000 shiftInsert" $ nf (foldl' (\ m n -> shiftInsert n () m) empty) [1..1000]
     , bench "1000 shiftDelete" $ nf (foldl' (flip shiftDelete) fixture1000) (replicate 1000 1)
-    , bench "shiftInsert on big" $ nf (shiftInsert 1 ()) fixture2to16
-    , bench "shiftDelete on big" $ nf (shiftDelete 1) fixture2to16
-    , bench "size on big" $ nf size fixture2to16
+    , bench "shiftInsert on big" $ nf (shiftInsert 1 ()) bigExample
+    , bench "shiftDelete on big" $ nf (shiftDelete 1) bigExample
+    , bench "size on big" $ nf size bigExample
     ]
