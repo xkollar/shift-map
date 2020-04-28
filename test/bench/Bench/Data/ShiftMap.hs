dc831db39630f59946237ca49818a1910519b531
Matej Kollar <208115@mail.muni.cz>
2020-04-28 16:20:00 +0100

    Observe how time is growing with more operations.

--- test/bench/Bench/Data/ShiftMap.hs
+++ test/bench/Bench/Data/ShiftMap.hs
@@ -30,13 +30,27 @@ setupEnv = pure fixture2to16
 fixture1000 :: ShiftMap ()
 fixture1000 = foldl' (\ m n -> shiftInsert n () m) empty [1..1000]
 
+fixture2000 :: ShiftMap ()
+fixture2000 = foldl' (\ m n -> shiftInsert n () m) empty [1..2000]
+
+fixture4000 :: ShiftMap ()
+fixture4000 = foldl' (\ m n -> shiftInsert n () m) empty [1..4000]
+
 fixture2to16 :: ShiftMap ()
 fixture2to16 = foldl' (\ m n -> shiftInsert n () m) empty [1..2^(16::Int)-1]
 
 benchmarks :: Benchmark
 benchmarks = env setupEnv $ \ bigExample -> bgroup "Bench.Data.ShiftMap"
-    [ bench "1000 shiftInsert" $ nf (foldl' (\ m n -> shiftInsert n () m) empty) [1..1000]
-    , bench "1000 shiftDelete" $ nf (foldl' (flip shiftDelete) fixture1000) (replicate 1000 1)
+    [ bgroup "shiftInsert"
+        [ bench "1000" $ nf (foldl' (\ m n -> shiftInsert n () m) empty) [1..1000]
+        , bench "2000" $ nf (foldl' (\ m n -> shiftInsert n () m) empty) [1..2000]
+        , bench "4000" $ nf (foldl' (\ m n -> shiftInsert n () m) empty) [1..4000]
+        ]
+    , bgroup "shiftDelete"
+        [ bench "1000" $ nf (foldl' (flip shiftDelete) fixture1000) (replicate 1000 1)
+        , bench "2000" $ nf (foldl' (flip shiftDelete) fixture2000) (replicate 2000 1)
+        , bench "4000" $ nf (foldl' (flip shiftDelete) fixture4000) (replicate 4000 1)
+        ]
     , bench "shiftInsert on big" $ nf (shiftInsert 1 ()) bigExample
     , bench "shiftDelete on big" $ nf (shiftDelete 1) bigExample
     , bench "size on big" $ nf size bigExample
